66a0a646a4bb3ee5072847cb68b317275f3009db
burhan <burhanatique@gmail.com>
2022-06-08 20:56:52 +0500

    swimming development

---
+++ js/script.js
@@ -0,0 +1,77 @@
+let navbarDiv = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
+        navbarDiv.classList.add('navbar-cng');
+    } else {
+        navbarDiv.classList.remove('navbar-cng');
+    }
+});
+
+
+const navbarCollapseDiv = document.getElementById('navbar-collapse');
+const navbarShowBtn = document.getElementById('navbar-show-btn');
+const navbarCloseBtn = document.getElementById('navbar-close-btn');
+// show navbar
+navbarShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-rmw');
+});
+
+// hide side bar
+navbarCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn"){
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+    }
+});
+
+// stop transition and animatino during window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+});
+
+
+
+$(document).ready(function(){
+
+
+$('.home-slider').owlCarousel({
+    items:1,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:8000,
+    loop:true
+});
+
+$('section').each(function(){
+
+    let height = $(this).height();
+    let offset = $(this).offset().top - 200;
+    let top = $(window).scrollTop();
+    let id = $(this).attr('id');
+
+    if(top >= offset && top < offset + height){
+        $('.navbar ul li a').removeClass('active');
+        $('.navbar').find(`[href="#${id}"]`).addClass('active');
+    }
+
+});
+
+$('.small-image img').click(function(){
+
+    $(this).addClass('image-active').siblings().removeClass('image-active');
+    let image = $(this).attr('src');
+    $('.big-image img').attr('src', image);
+
+});
+
+
+});
